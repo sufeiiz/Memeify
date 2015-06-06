a7972904cea7d621b276a18d333de597b0a187ff
ahhhlvin <alvkuang@gmail.com>
2015-06-06 16:52:29 -0400

    fixed stuff

--- Memer/app/src/main/java/nyc/c4q/scar/memer/SecondActivity.java
+++ Memer/app/src/main/java/nyc/c4q/scar/memer/SecondActivity.java
@@ -1,32 +1,34 @@
 package nyc.c4q.scar.memer;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by sufeizhao on 5/31/15.
  */
-public class SecondActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity implements Serializable {
 
     private ViewSwitcher viewSwitcher;
     private Uri uri;
@@ -35,56 +37,89 @@ public class SecondActivity extends AppCompatActivity {
     private ImageView imageView2;
     private String stringVariable = "file:///sdcard/_pictureholder_id.jpg";
     private boolean isVanilla = true;
+    private float fontSize;
+    private String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    private SharedPreferences preferences = null;
+    public final String IMAGE_FILE = "image_file";
+    public static Bitmap bm;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-
-
+        preferences = this.getSharedPreferences(IMAGE_FILE, Context.MODE_PRIVATE);
 
         //This loads up the last saved boolean for which layout mode was selected
         if (savedInstanceState != null) {
             isVanilla = (boolean) savedInstanceState.get("isVanilla");
-
         }
-
-        //This sets the layout according to which layout mode is selected
-        if (isVanilla) {
-            Typeface impact = Typeface.createFromAsset(getAssets(), "Impact.ttf");
-            nyc.c4q.scar.memer.AutoResizeEditText resizeTop = (nyc.c4q.scar.memer.AutoResizeEditText) findViewById(R.id.top);
-            nyc.c4q.scar.memer.AutoResizeEditText resizeBottom = (nyc.c4q.scar.memer.AutoResizeEditText) findViewById(R.id.bottom);
-            resizeTop.setTypeface(impact);
-            resizeBottom.setTypeface(impact);
-        }
-
 
         viewSwitcher = (ViewSwitcher) findViewById(R.id.viewswitcher);
-
         imageView = (ImageView) findViewById(R.id.insert_pic_id);
         imageView2 = (ImageView) findViewById(R.id.insert_pic_id2);
         ImageButton changeImage = (ImageButton) findViewById(R.id.change_img);
         ImageButton shareImage = (ImageButton) findViewById(R.id.share);
-
+        ImageButton saveImage = (ImageButton) findViewById(R.id.save);
+        final EditText top = (EditText) findViewById(R.id.top);
+        final EditText bottom = (EditText) findViewById(R.id.bottom);
+        //This loads up dialog
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showListViewDialog();
+            }
+        });
 
         shareImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent shareIntent = new Intent();
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("image_file", MODE_PRIVATE );
+                editor.commit();
+
+                SharedPreferences prefs = getSharedPreferences("image_file", MODE_PRIVATE);
                 shareIntent.setAction(Intent.ACTION_SEND);
                 shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
                 shareIntent.setType("image/jpeg");
+//                startActivity(Intent.createChoosershareIntent, getResources().getText(R.string.app_name));
                 startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.app_name)));
-                //File file = createImageFile();
 
             }
-
-
         });
 
+//        shareImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent shareIntent = new Intent();
+//                shareIntent.setAction(Intent.ACTION_SEND);
+//                shareIntent.putExtra(Intent.EXTRA_STREAM, MediaStore.Images.Media.getContentUri("image" + timeStamp + ".jpg"));
+//                shareIntent.setType("image/jpeg");
+//                startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.app_name)));
+//            }
+//        });
 
+        // Hides editText is nothing has been entered, brings it back after image has been saved
+        saveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if (top.getText().toString().matches("")) {
+//                    top.setVisibility(View.GONE);
+//                }
+//                if (bottom.getText().toString().matches("")) {
+//                    bottom.setVisibility(View.GONE);
+//                }
+
+                View v1 = viewSwitcher.getCurrentView();
+                v1.setDrawingCacheEnabled(true);
+                Bitmap bm = v1.getDrawingCache();
+                MediaStore.Images.Media.insertImage(getContentResolver(), bm, "image" + timeStamp + ".jpg", timeStamp.toString());
+                Toast.makeText(getApplicationContext(), "Image was saved", Toast.LENGTH_SHORT).show();
+
+                top.setVisibility(View.VISIBLE);
+                bottom.setVisibility(View.VISIBLE);
+            }
+        });
 
         Button switcherButton = (Button) findViewById(R.id.switcherButton);
         switcherButton.setOnClickListener(new View.OnClickListener() {
@@ -95,33 +130,28 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
         //This loads up any existing savedInstanceStates
         if (savedInstanceState != null) {
             uri = (Uri) savedInstanceState.get("luckyM");
-            imageView.setImageURI(uri);
-            imageView2.setImageURI(uri);
         } else {
             uri = (Uri) getIntent().getExtras().get("luckyM");
-            imageView.setImageURI(uri);
-            imageView2.setImageURI(uri);
         }
+        imageView.setImageURI(uri);
+        imageView2.setImageURI(uri);
+//        fontSize = getImageSize(uri) ;
 
-        //This loads up dialog
-        changeImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showListViewDialog();
-            }
-        });
-//
+        //This sets the layout according to which layout mode is selected
+        if (isVanilla) {
+            Typeface impact = Typeface.createFromAsset(getAssets(), "Impact.ttf");
+            top.setTypeface(impact);
+            bottom.setTypeface(impact);
 
+
+        } else {
+            top.setTypeface(Typeface.create("serif", Typeface.NORMAL));
+            bottom.setTypeface(Typeface.create("serif", Typeface.NORMAL));
+        }
     }
-
 
     //This handles the activity for the intent: using the camera and choosing from a gallery.
     @Override
@@ -131,17 +161,18 @@ public class SecondActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
             uri = data.getData();
             imageView.setImageURI(uri);
+            imageView2.setImageURI(uri);
+//            fontSize = getImageSize(uri) / 40;
+
         }
 
         if (requestCode == 0 && resultCode == RESULT_OK) {
-
             uri = Uri.parse("file:///sdcard/picture.jpg");
-            imageView.setImageURI(null);
             imageView.setImageURI(Uri.parse(stringVariable));
-
+            imageView2.setImageURI(Uri.parse(stringVariable));
+//            fontSize = getImageSize(Uri.parse(stringVariable)) / 40;
         }
     }
-
 
     //This is for the dialog box: Camera or Gallery
     private void showListViewDialog() {
@@ -157,8 +188,6 @@ public class SecondActivity extends AppCompatActivity {
 
                     Uri imageFileUri = Uri.parse(stringVariable);
                     intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri);
-
-
                     if (intent.resolveActivity(getPackageManager()) != null) {
                         startActivityForResult(intent, 0);
 
@@ -181,34 +210,14 @@ public class SecondActivity extends AppCompatActivity {
         super.onSaveInstanceState(toSave);
         toSave.putParcelable("luckyM", uri);
         toSave.putBoolean("isVanilla", isVanilla);
-        //TODO: save viewswitcher??
     }
 
-    private File createImageFile() throws IOException {
-        String mCurrentPhotoPath;
-
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
+    // gets image height to adjust edittext accordingly
     public int getImageSize(Uri uri) {
         BitmapFactory.Options o = new BitmapFactory.Options();
         o.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(getAbsolutePath(uri), o);
-        int width = o.outWidth;
-        return width;
+        return o.outHeight;
     }
 
     public String getAbsolutePath(Uri uri) {
@@ -222,4 +231,5 @@ public class SecondActivity extends AppCompatActivity {
         } else
             return null;
     }
+
 }
